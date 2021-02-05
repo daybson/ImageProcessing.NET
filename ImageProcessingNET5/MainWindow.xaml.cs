22bd792bb8a7d70b17a7322221f6f23643849425
Daybson B. Salles Paisante <daybson.paisante@outlook.com>
2021-02-04 21:38:23 -0300

    * load/convert image

---
+++ ImageProcessingNET5/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using ImageProcessingNET5.src;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageProcessingNET5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Stopwatch a = new Stopwatch();
+
+            var bmp = new BitmapImage(new Uri("D://lena.jpg"));
+            imgSource.Source = bmp;
+
+            a.Start();
+            int stride = bmp.Format.BitsPerPixel / 8 * bmp.PixelWidth;
+
+            var pixels = new byte[stride * bmp.PixelHeight];
+
+            bmp.CopyPixels(pixels, stride, 0);
+
+            CImage cImage = new CImage(bmp.PixelWidth, bmp.PixelHeight, bmp.Format.BitsPerPixel, pixels);
+
+            WriteableBitmap writeableBitmap = new WriteableBitmap(bmp.PixelWidth, bmp.PixelHeight, bmp.DpiX, bmp.DpiY, bmp.Format, null);
+
+            writeableBitmap.WritePixels(new Int32Rect(0, 0, bmp.PixelWidth, bmp.PixelHeight), cImage.Grid, stride, 0);
+
+            a.Stop();
+
+            imgResult.Source = writeableBitmap;
+
+            lblTime.Content = a.ElapsedMilliseconds + "ms";
+        }
+    }
+}
---
+++ ImageProcessingNET5/src/CImage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcessingNET5.src
+{
+    public class CImage
+    {
+        public byte[] Grid;
+        public int width, height, nBits;
+
+        public CImage() { }
+
+        /// <summary>
+        /// Inicializa com array de bytes vazio, porém, de tamanho determinado.
+        /// </summary>
+        public CImage(int nx, int ny, int nbits) 
+        {
+            width = nx;
+            height = ny;
+            nBits = nbits;
+            Grid = new byte[width * height * (nBits / 8)];
+        }
+
+
+        /// <summary>
+        /// Inicializa fazendo a cópia byte a byte do array passado como parâmetro.
+        /// </summary>
+        /// <param name="nbits">Total de bits por pixel (canal * depth)</param>
+        public CImage(int nx, int ny, int nbits, byte[] img) 
+        {
+            width = nx;
+            height = ny;
+            nBits = nbits;
+            Grid = new byte[width * height * (nBits / 8)];
+            
+            int max = width * height * nBits / 8;
+
+            for (int i = 0; i < max; i++)
+            {
+                Grid[i] = img[i];
+            }
+        }
+    }
+}
